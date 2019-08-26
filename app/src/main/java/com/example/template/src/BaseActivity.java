8950c94d955441f2fde8b20315600ce742359e5f
hsj321 <randy3456@naver.com>
2019-08-26 20:19:43 +0900

    로그인화면 80%(로그인버튼 활성화 시 버튼크기 불안정)

--- app/src/main/java/com/example/template/src/BaseActivity.java
+++ app/src/main/java/com/example/template/src/BaseActivity.java
@@ -12,6 +12,44 @@ import com.example.herethereproject.R;
 public class BaseActivity extends AppCompatActivity {
     public ProgressDialog mProgressDialog;
 
+    //올바른 비밀번호 입력 확인(대소문자 포함)
+    public boolean checkPassword(String password){
+        boolean checkNumber = false;
+        boolean checkEnglish = false;
+        if(!(password.length() >= 6 && password.length() <=10)){
+            return false;
+        } else {
+            for(int i = 0; i < password.length(); i++){
+                if(checkNumber && checkEnglish){
+                    return true;
+                }
+                if(((int)password.charAt(i) >= 97 && (int)password.charAt(i) <= 122) || (int)password.charAt(i) >=65 && (int)password.charAt(i) <= 90){
+                    //영문이 포함되 있을 때(대소문자)
+                    checkEnglish = true;
+                } else if((int)password.charAt(i) >=48 && (int)password.charAt(i) <= 57){
+                    //숫자가 포함되있을 때
+                    checkNumber = true;
+                } else {
+                    //영어 숫자 외의 문자가 있을 때
+                    return false;
+                }
+            }
+            return false;
+        }
+    }
+    //올바른 이메일 입력 확인
+    public  boolean checkEmail(String email){
+        if(!email.contains("@")){
+            return false;
+        } else {
+            if(!(email.charAt(email.length()-4) == '.')){
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+
     public void showCustomToast(final String message) {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
     }
--- app/src/main/java/com/example/template/src/login/LoginActivity.java
+++ app/src/main/java/com/example/template/src/login/LoginActivity.java
@@ -3,21 +3,63 @@ package com.example.template.src.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.Html;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import com.example.herethereproject.R;
 import com.example.template.src.BaseActivity;
 import com.example.template.src.login.interfaces.LoginActivityView;
 
+import org.w3c.dom.Text;
+
 public class LoginActivity extends BaseActivity implements LoginActivityView {
+
+    EditText et_email;
+    EditText et_password;
+    ImageButton btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
+        et_email = findViewById(R.id.login_email_input);
+        et_password = findViewById(R.id.login_password_input);
+        btn_login = findViewById(R.id.login_button);
+
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (checkPassword(et_password.getText().toString()) && checkEmail(et_email.getText().toString())) {
+                    //올바른 입력
+                    System.out.println("success");
+                    btn_login.setImageDrawable(getDrawable(R.drawable.ic_login_yes));
+                } else {
+                    //잘못된 입력
+                    System.out.println("fail");
+                    btn_login.setImageDrawable(getDrawable(R.drawable.ic_login_none));
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        };
+
+        et_email.addTextChangedListener(textWatcher);
+        et_password.addTextChangedListener(textWatcher);
 
 
     }
